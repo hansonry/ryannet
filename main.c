c18a36c62862f33757c7d181e2032d6a3379fcba
Ryan Hanson <hansonry@gmail.com>
2018-07-08 12:08:59 -0500

    Added a test that I could run two instances

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #define PORT "1234"
-int main(int args, char * argc[])
+int main(int argc, char * args[])
 {
    struct ryannet_socket_tcp * client_socket, * server_socket, * con;
    struct ryannet_address * addy;
@@ -37,8 +37,45 @@ int main(int args, char * argc[])
    
    
    ryannet_socket_tcp_destroy(con);
-   ryannet_socket_tcp_destroy(client_socket);
    ryannet_socket_tcp_destroy(server_socket);
+   ryannet_socket_tcp_destroy(client_socket);
+
+   sprintf(buffer, "This is not the message you want");
+
+   if( argc == 3)
+   {
+      if(args[1][0] == 's')
+      {
+         server_socket = ryannet_socket_tcp_new();
+         ryannet_socket_tcp_bind(server_socket, NULL, args[2]);
+         con = NULL;
+         while(con == NULL)
+         {
+            con = ryannet_socket_tcp_accept(server_socket);
+         }
+
+         size = sprintf(buffer, "Hey, Whazzup!") + 1;
+         ryannet_socket_tcp_send(con, buffer, size);
+         ryannet_socket_tcp_destroy(con);
+         ryannet_socket_tcp_destroy(server_socket);
+      }
+      else
+      {
+         client_socket = ryannet_socket_tcp_new();
+         ryannet_socket_tcp_connect(client_socket, args[1], args[2]);
+         size = 0;
+         while(size == 0)
+         {
+            size = ryannet_socket_tcp_receive(client_socket, buffer, 255);
+         }
+
+         printf("Message Length %d, Messgee: %s\n", size, buffer);
+         ryannet_socket_tcp_destroy(client_socket);
+
+      }
+   }
+
+
    ryannet_destroy();
    printf("End\n");
    return 0;
